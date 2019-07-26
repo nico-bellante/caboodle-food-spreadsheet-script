eed9995b1e55a8466bf25c484ebb7f910bcb6016
Nico Bellante <nicobell@microsoft.com>
2019-07-26 14:12:42 -0700

    changes to chef

--- food-spreadsheet/index.ts
+++ food-spreadsheet/index.ts
@@ -1,20 +1,20 @@
 const CONSTANTS = {
   SHEET_NAMES: {
-    BoardingSchedule: 'Boarding Schedule',
-    AllCatsInStore: '_private_all_cats_at_store',
-  },
+    BoardingSchedule: "Boarding Schedule",
+    AllCatsInStore: "_private_all_cats_at_store"
+  }
 };
 
 type BoardingScheduleSchema = {
-  'Cat Name': string;
-  'Start Date': Date;
-  'End Date': Date;
-  'Dietary Restrictions': string;
-  'Other Notes': string;
+  "Cat Name": string;
+  "Start Date": Date;
+  "End Date": Date;
+  "Dietary Restrictions": string;
+  "Other Notes": string;
 };
 
 type CatSchema = {
-  adoptOrBoard: 'Adopt' | 'Board';
+  adoptOrBoard: "Adopt" | "Board";
   name: string;
   startDate: Date;
   endDate: Date;
@@ -24,19 +24,24 @@ type CatSchema = {
 };
 
 function getFoodOrderForm() {
-  Logger.log('starting');
+  Logger.log("starting");
   var form = FormApp.openByUrl(
-    'https://docs.google.com/forms/d/1GG1NsHHXGsgI1sAXxJyXW9hZ-ZCGgYBx1bdcBSAPmqs/edit',
+    "https://docs.google.com/forms/d/1GG1NsHHXGsgI1sAXxJyXW9hZ-ZCGgYBx1bdcBSAPmqs/edit"
   );
+  form.deleteAllResponses();
   form.getItems().forEach(item => form.deleteItem(item));
-  // form.setTitle('Today Feeding');
+  form.setTitle("Food Distribution Form");
+  form
+    .addTextItem()
+    .setTitle("Chef for 7/20/2019 AM")
+    .setRequired(true);
 
-  Logger.log('form opened');
+  Logger.log("form opened");
 
   getAllCatsInStore()
     .map(
       (
-        cat: CatSchema,
+        cat: CatSchema
       ): {
         name: string;
         dietRestrictions?: string;
@@ -46,26 +51,29 @@ function getFoodOrderForm() {
         Logger.log(`processing: ${cat.name}`);
         return {
           name: cat.name,
-          dietRestrictions: 'TODO DIET RESTRICTION',
-          otherNotes: 'I AM OTHER NOTE',
+          dietRestrictions: "TODO DIET RESTRICTION",
+          otherNotes: "I AM OTHER NOTE",
           favoriteFoods: {
-            'Food A': { yes: 10, no: 2 },
-            'Food B': { yes: 8, no: 3 },
-            'Food C': { yes: 5, no: 4 },
-          },
+            "Food A": { yes: 10, no: 2 },
+            "Food B": { yes: 8, no: 3 },
+            "Food C": { yes: 5, no: 4 }
+          }
         };
-      },
+      }
     )
     .forEach(data => {
       form.addPageBreakItem().setTitle(data.name);
       form
         .addImageItem()
-        .setTitle('Food for past X')
+        .setTitle("Food for past X")
         .setImage(makeYesNoBarGraph(data.favoriteFoods).getBlob());
-      form.addTextItem().setTitle(`${data.name}'s Food`);
+      form
+        .addTextItem()
+        .setTitle(`${data.name}'s Food`)
+        .setRequired(true);
       // form.addParagraphTextItem().setTitle(`Other Notes For ${data.name}`);
     });
-  Logger.log('done with iteration');
+  Logger.log("done with iteration");
   Logger.log(form.getEditUrl());
 
   Logger.log(form.getPublishedUrl());
@@ -76,45 +84,52 @@ function testMakeChart() {}
 /////////////////////////////////////////////////////////////
 function getAllCatsInStore() {
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(
-    CONSTANTS.SHEET_NAMES.AllCatsInStore,
+    CONSTANTS.SHEET_NAMES.AllCatsInStore
   );
   return getTableDataFromSheetAsArrOfObj<CatSchema>(sheet).filter(({ name }) =>
-    Boolean(name),
+    Boolean(name)
   );
 }
 
 function getTableDataFromSheetAsArrOfObj<T extends {}>(
-  sheet: GoogleAppsScript.Spreadsheet.Sheet,
+  sheet: GoogleAppsScript.Spreadsheet.Sheet
 ): T[] {
   const [headers, ...data] = sheet.getDataRange().getValues();
-  return data.map(row => headers.reduce((o, header, i) => ({ ...o, [header]: row[i] })));
+  return data.map(row =>
+    headers.reduce((o, header, i) => ({ ...o, [header]: row[i] }))
+  );
 }
 
 type FoodHistoryData = {
   [foodName: string]: { yes: number; no: number };
 };
 
-function makeYesNoBarGraph(data: FoodHistoryData): GoogleAppsScript.Charts.Chart {
-  const maxTotalFeedings = Object.keys(data).reduce((max: number, foodName: string) => {
-    const thisRow = data[foodName];
-    return Math.max(
-      max,
-      Object.keys(thisRow)
-        .map(k => thisRow[k])
-        .reduce((a, b) => a + b, 0),
-    );
-  }, 0);
+function makeYesNoBarGraph(
+  data: FoodHistoryData
+): GoogleAppsScript.Charts.Chart {
+  const maxTotalFeedings = Object.keys(data).reduce(
+    (max: number, foodName: string) => {
+      const thisRow = data[foodName];
+      return Math.max(
+        max,
+        Object.keys(thisRow)
+          .map(k => thisRow[k])
+          .reduce((a, b) => a + b, 0)
+      );
+    },
+    0
+  );
   let dataTableBuilder = Charts.newDataTable()
-    .addColumn(Charts.ColumnType.STRING, 'Month')
-    .addColumn(Charts.ColumnType.NUMBER, 'Yes')
+    .addColumn(Charts.ColumnType.STRING, "Month")
+    .addColumn(Charts.ColumnType.NUMBER, "Yes")
     // .addColumn(Charts.ColumnType.NUMBER, 'Half')
-    .addColumn(Charts.ColumnType.NUMBER, 'No');
+    .addColumn(Charts.ColumnType.NUMBER, "No");
 
   Object.keys(data).forEach(foodName => {
     dataTableBuilder = dataTableBuilder.addRow([
       foodName,
       data[foodName].yes,
-      data[foodName].no,
+      data[foodName].no
     ]);
   });
 
@@ -122,7 +137,7 @@ function makeYesNoBarGraph(data: FoodHistoryData): GoogleAppsScript.Charts.Chart
     .setDataTable(dataTableBuilder as GoogleAppsScript.Charts.DataTableBuilder)
     .setStacked()
     .setRange(0, maxTotalFeedings)
-    .setColors(['green', 'red'])
+    .setColors(["green", "red"])
     .build();
 
   // const imageData = Utilities.base64Encode(chart.getAs('image/png').getBytes());
