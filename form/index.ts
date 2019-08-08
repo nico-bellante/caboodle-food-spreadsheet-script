782f6542be53d414f4e50800991ed9ffab717ac2
Nico Bellante <nicobell@microsoft.com>
2019-08-08 15:56:21 -0700

    working form, needs polish

--- form/index.ts
+++ form/index.ts
@@ -1,7 +1,7 @@
 function setupRecordingForm() {
   const form = FormApp.getActiveForm();
-  let sheet: GoogleAppsScript.Spreadsheet.Sheet = SpreadsheetApp.openById(
-    "1gQNMd06hj6Lsh1OPWKC1L0wgc12aqiNroR97XV-D2cI",
+  const sheet: GoogleAppsScript.Spreadsheet.Sheet = SpreadsheetApp.openById(
+    "1gQNMd06hj6Lsh1OPWKC1L0wgc12aqiNroR97XV-D2cI"
   ).getSheetByName("Feeding Logs");
 
   form.deleteAllResponses();
@@ -14,10 +14,10 @@ function setupRecordingForm() {
       const prettyDate = convertDateAmPmIntoPrettyDate(item.date, item.amPM);
       return {
         ...byPrettyDate,
-        [prettyDate]: [...(byPrettyDate[prettyDate] || []), item],
+        [prettyDate]: [...(byPrettyDate[prettyDate] || []), item]
       };
     },
-    {} as { [prettyDate: string]: FeedingLogItem[] },
+    {} as { [prettyDate: string]: FeedingLogItem[] }
   );
 
   Object.keys(byPrettyDate).forEach(prettyDate => {
@@ -27,19 +27,121 @@ function setupRecordingForm() {
       .setRows(
         byPrettyDate[prettyDate].reduce(
           (all, item) => {
-            return [...all, ...item.foods.map(({ name }) => `${item.catName} - ${name}`)];
+            return [
+              ...all,
+              ...item.foods.map(({ name }) => `${item.catName} - ${name}`)
+            ];
           },
-          [] as string[],
-        ),
+          [] as string[]
+        )
       )
       .setColumns(["Yes", "Half", "No"])
       .setRequired(true);
   });
 }
 
+type IResponse = { question: string; answer: string[]; rows: string[] };
+type FoodResponseStatus = "Yes" | "No" | "Half";
+function onFormSubmit(event: any) {
+  const responses: IResponse[] = event.response
+    .getItemResponses()
+    .map((itemResponse: any) => ({
+      question: itemResponse.getItem().getTitle(),
+      answer: itemResponse.getResponse(),
+      rows: itemResponse
+        .getItem()
+        .asGridItem()
+        .getRows()
+    }));
+
+  const byPrettyDate: {
+    [prettyDate: string]: {
+      [catName: string]: {
+        [foodName: string]: FoodResponseStatus;
+      };
+    };
+  } = responses.reduce((byPrettyDate, response) => {
+    const prettyDate = response.question.replace(" Feeding", "");
+    const results = response.rows
+      .map((row, i) => {
+        const [match, catName, foodName] = row.match(/(.*)\s\-\s(.*)/)!;
+        return {
+          catName,
+          foodName,
+          foodStatus: response.answer[i] as FoodResponseStatus
+        };
+      })
+      .reduce(
+        (byCatName, item) => ({
+          ...byCatName,
+          [item.catName]: {
+            ...byCatName[item.catName],
+            [item.foodName]: item.foodStatus
+          }
+        }),
+        {} as { [catName: string]: { [foodName: string]: FoodResponseStatus } }
+      );
+
+    return { ...byPrettyDate, [prettyDate]: results };
+  }, {});
+
+  addLogToLogSheet(responses, "responses");
+  addLogToLogSheet(byPrettyDate, "byprettydate");
+
+  const sheet: GoogleAppsScript.Spreadsheet.Sheet = SpreadsheetApp.openById(
+    "1gQNMd06hj6Lsh1OPWKC1L0wgc12aqiNroR97XV-D2cI"
+  ).getSheetByName("Feeding Logs");
+
+  const existingData = sheet
+    .getRange("B3:D")
+    .getValues()
+    .map((row, i) => ({ i, date: row[0], amPM: row[1], catName: row[2] }));
+
+  Object.keys(byPrettyDate).forEach(prettyDate => {
+    const data = byPrettyDate[prettyDate];
+    const { date, amPM } = convertPrettyDateIntoDateAmPm(prettyDate);
+    Object.keys(data).forEach(catName => {
+      existingData
+        .filter(
+          existingItem =>
+            existingItem.amPM === amPM &&
+            getDateStringFromDate(existingItem.date) ===
+              getDateStringFromDate(date) &&
+            existingItem.catName === catName
+        )
+        .map(({ i }) => i + 3)
+        .forEach(i => {
+          const existingItem = getRowOfFeedingLog(sheet, i);
+          addLogToLogSheet(existingItem, `existing item ${i}`);
+          Object.keys(data[catName]).forEach(foodName => {
+            const newStatus = data[catName][foodName].charAt(0);
+            switch (foodName) {
+              case existingItem.food1:
+                existingItem.food1Status = newStatus;
+                break;
+              case existingItem.food2:
+                existingItem.food2Status = newStatus;
+                break;
+              case existingItem.food3:
+                existingItem.food3Status = newStatus;
+                break;
+              case existingItem.food4:
+                existingItem.food4Status = newStatus;
+                break;
+            }
+          });
+          addLogToLogSheet(existingItem, `existing item after ${i}`);
+          writeFeedingLogRowOfData(sheet, i, { ...existingItem });
+        });
+    });
+  });
+}
+
 //////////////////////////////////////////////////////
 
-function getAllFeedingsWithQuestionMarks(sheet: GoogleAppsScript.Spreadsheet.Sheet) {
+function getAllFeedingsWithQuestionMarks(
+  sheet: GoogleAppsScript.Spreadsheet.Sheet
+) {
   return sheet
     .getRange("E3:E")
     .getValues()
@@ -60,7 +162,7 @@ type FeedingLogItem = {
 
 function getFeedingLogItem(
   sheet: GoogleAppsScript.Spreadsheet.Sheet,
-  index: number,
+  index: number
 ): FeedingLogItem {
   const {
     timestamp,
@@ -75,7 +177,7 @@ function getFeedingLogItem(
     food3,
     food3Status,
     food4,
-    food4Status,
+    food4Status
   } = getRowOfFeedingLog(sheet, index);
   const foods = [];
 
@@ -98,7 +200,7 @@ function getFeedingLogItem(
     amPM,
     catName,
     status,
-    foods,
+    foods
   };
 }
 
@@ -124,7 +226,7 @@ type FeedingLogRowOfData = {
 
 function getRowOfFeedingLog(
   sheet: GoogleAppsScript.Spreadsheet.Sheet,
-  index: number,
+  index: number
 ): FeedingLogRowOfData {
   // addLogToLogSheet(sheet.getRange(index, 1, 1, 13).getValues()[0], "feedingrowlog");
   const [
@@ -140,7 +242,7 @@ function getRowOfFeedingLog(
     food3,
     food3Status,
     food4,
-    food4Status,
+    food4Status
   ] = sheet.getRange(index, 1, 1, 13).getValues()[0];
 
   const status = sheet.getRange(index, 5).getFormula();
@@ -157,7 +259,7 @@ function getRowOfFeedingLog(
     food3,
     food3Status,
     food4,
-    food4Status,
+    food4Status
   };
 }
 
@@ -177,8 +279,8 @@ function writeFeedingLogRowOfData(
     food3,
     food3Status,
     food4,
-    food4Status,
-  }: FeedingLogRowOfData,
+    food4Status
+  }: FeedingLogRowOfData
 ): void {
   sheet
     .getRange(index, 1, 1, 13)
@@ -196,8 +298,8 @@ function writeFeedingLogRowOfData(
         food3,
         food3Status,
         food4,
-        food4Status,
-      ],
+        food4Status
+      ]
     ]);
 }
 
@@ -227,22 +329,41 @@ function prepareFeedingRowData(data: RealFeedingRowData) {
     data.food3 || "--",
     getQuestionMarkOrDash(data.food3),
     data.food4 || "--",
-    getQuestionMarkOrDash(data.food4),
+    getQuestionMarkOrDash(data.food4)
   ];
 }
 
 function convertDateAmPmIntoPrettyDate(date: Date, amPM: "AM" | "PM"): string {
   return (
-    date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear() + " " + amPM
+    date.getMonth() +
+    1 +
+    "/" +
+    date.getDate() +
+    "/" +
+    date.getFullYear() +
+    " " +
+    amPM
   );
 }
 
 function convertPrettyDateIntoDateAmPm(
-  prettyDate: string,
+  prettyDate: string
 ): { date: Date; amPM: "AM" | "PM" } {
   const dateRegex = /(\d\d?\s*\/\s*\d\d?\s*\/\s*\d\d\d\d)\s+(AM|PM)/;
   const match = prettyDate.match(dateRegex);
   const date = new Date(match![1]);
   const amPM = match![2] as "AM" | "PM";
   return { date, amPM };
+}
+
+let LOG_COUNT = 1;
+function addLogToLogSheet(obj: any, note?: string) {
+  const LOG_SHEET = SpreadsheetApp.openById(
+    "1gQNMd06hj6Lsh1OPWKC1L0wgc12aqiNroR97XV-D2cI"
+  ).getSheetByName("Logs");
+  const THIS_LOG_NO = LOG_COUNT++;
+  LOG_SHEET.getRange(THIS_LOG_NO, 1).setValue(JSON.stringify(obj, null, 4));
+  if (note) {
+    LOG_SHEET.getRange(THIS_LOG_NO, 2).setValue(note);
+  }
 }
